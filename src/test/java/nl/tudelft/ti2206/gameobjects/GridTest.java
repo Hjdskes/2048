7966700ec09602a7f5094c87143d031ca4472a4d
paulbakker65 <paulbakker65@gmail.com>
2014-09-06 09:01:26 +0200

    GridTest initial tests

---
+++ src/test/java/nl/tudelft/ti2206/gameobjects/GridTest.java
@@ -0,0 +1,72 @@
+package nl.tudelft.ti2206.gameobjects;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import nl.tudelft.ti2206.game.GameWorld;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+public class GridTest {
+
+	Grid grid;
+	@Mock
+	GameWorld world;
+	
+	@Before
+	public void setup(){
+		grid = new Grid(world);
+	}
+	
+	@Test
+	public void testConstructor() {
+		assertNotNull(grid);
+	}
+	
+	@Test
+	public void testConstructor2() {
+		int TWO = 0;
+		int FOUR = 0;
+		for(int i = 0; i < 16; i++) {
+			if(grid.getTiles()[i].getValue() == 2) {
+				TWO++;
+			}
+			if(grid.getTiles()[i].getValue() == 4) {
+				FOUR++;
+			}	
+		}
+		assertEquals(TWO + FOUR, 2);
+	}
+	
+	@Test
+	public void testHighest(){
+		int empty = grid.getRandomEmptyLocation();
+		grid.getTiles()[empty].setValue(1024);
+		int empty2 = grid.getRandomEmptyLocation();
+		grid.getTiles()[empty2].setValue(1025);
+		grid.updateHighest();
+		assertEquals(grid.getHighest(), 1025);
+	}
+	
+	@Test
+	public void testIsFull(){
+		for(int i = 0; i < 16; i++){
+			grid.getTiles()[i].setValue(2);
+		}
+		assertTrue(grid.isFull());
+		
+	}
+	
+	@Test
+	public void testIsFull2(){
+		for(int i = 0; i < 16; i++){
+			grid.getTiles()[i].setValue(2);
+			i++;
+		}
+		assertFalse(grid.isFull());
+	}
+	
+}
