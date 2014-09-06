7c912530d1f44c1c86aa080936377775ebc677db
Jochem <J.W.Heijltjes@student.tudelft.nl>
2014-09-06 19:08:46 +0200

    Restored heading comment in Grid class.

--- src/main/java/nl/tudelft/ti2206/gameobjects/Grid.java
+++ src/main/java/nl/tudelft/ti2206/gameobjects/Grid.java
@@ -16,8 +16,14 @@ import nl.tudelft.ti2206.helpers.TileMover;
  * 
  * For example, imagine the grid being laid out like this:
  * 
- * +---+---+---+---+ | 0 | 1 | 2 | 3 | +---+---+---+---+ | 4 | 5 | 6 | 7 |
- * +---+---+---+---+ | 8 | 9 | 10| 11| +---+---+---+---+ | 12| 13| 14| 15|
+ * +---+---+---+---+ 
+ * | 0 | 1 | 2 | 3 | 
+ * +---+---+---+---+ 
+ * | 4 | 5 | 6 | 7 |
+ * +---+---+---+---+ 
+ * | 8 | 9 | 10| 11| 
+ * +---+---+---+---+ 
+ * | 12| 13| 14| 15|
  * +---+---+---+---+
  * 
  * Now, a square on field 10 can move left or right by adding or subtracting 1
