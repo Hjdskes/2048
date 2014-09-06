f84cab166c7ea2c5dfa7b421016215d35e513816
Jochem <J.W.Heijltjes@student.tudelft.nl>
2014-09-06 14:07:33 +0200

    Added 2048 icons for several operating systems.

--- src/main/java/nl/tudelft/ti2206/game/Launcher.java
+++ src/main/java/nl/tudelft/ti2206/game/Launcher.java
@@ -2,6 +2,8 @@ package nl.tudelft.ti2206.game;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
@@ -20,23 +22,34 @@ public class Launcher {
 
 	/**
 	 * Main entry point for the game.
+	 * 
 	 * @param args
-	 *             command line arguments. These
-	 *             are ignored.
+	 *            command line arguments. These are ignored.
 	 */
 	public static void main(final String[] args) {
 		// This main method is called when starting your game.
 		LOG.info("Starting game...");
 
-		LwjglApplicationConfiguration config =
-				new LwjglApplicationConfiguration();
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
 		config.title = "2048";
+
 		config.width = WIDTH;
 		config.height = HEIGHT;
+		
+		// check if we're running on Mac OS and load the appropriate icon
+		if (System.getProperty("os.name").toLowerCase().indexOf("mac") >= 0)
+			config.addIcon(
+					"src/main/resources/images/icons/2048_mac.png",
+					FileType.Internal);
+		// otherwise load icon for windows/linux
+		else
+			config.addIcon(
+					"src/main/resources/images/icons/2048_linux_windows.png",
+					FileType.Internal);
 
-		LOG.info("Window width and height: " + config.width
-				+ "x" + config.height);
+		LOG.info("Window width and height: " + config.width + "x"
+				+ config.height);
 		new LwjglApplication(new TwentyFourtyGame(), config);
 	}
 }
-
