61f80d56b053c3647b554979fbe64e2c57c4afb7
Jente Hidskes <hjdskes@gmail.com>
2014-09-10 21:57:08 +0200

    Make RestartButton work with the new abstract SimpleButton

--- src/main/java/nl/tudelft/ti2206/buttons/RestartButton.java
+++ src/main/java/nl/tudelft/ti2206/buttons/RestartButton.java
@@ -5,16 +5,31 @@ import nl.tudelft.ti2206.game.GameWorld.GameState;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
+/**
+ * 
+ * @author group-21
+ */
 public class RestartButton extends SimpleButton {
-
+	/** */
 	private GameWorld world;
-	
+
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @param width
+	 * @param height
+	 * @param buttonUp
+	 * @param buttonDown
+	 * @param world
+	 */
 	public RestartButton(float x, float y, float width, float height,
 			Sprite buttonUp, Sprite buttonDown, GameWorld world) {
 		super(x, y, width, height, buttonUp, buttonDown);
 		this.world = world;
 	}
 
+	@Override
 	public void onClick() {
 		world.setGameState(GameState.RUNNING);
 		world.restart();
