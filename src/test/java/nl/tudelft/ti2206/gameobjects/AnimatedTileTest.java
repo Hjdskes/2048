29b28271c6e6e80b48bae53f1954060b502b85a7
Jochem <J.W.Heijltjes@student.tudelft.nl>
2014-09-13 16:59:59 +0200

    Added AnimatedGridTest and AnimatedTileTest test cases.

---
+++ src/test/java/nl/tudelft/ti2206/gameobjects/AnimatedTileTest.java
@@ -0,0 +1,75 @@
+package nl.tudelft.ti2206.gameobjects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test suite for the AnimatedTile class.
+ *
+ * @author group-21
+ *
+ */
+public class AnimatedTileTest {
+	private AnimatedTile tile;
+
+	@Before
+	public void setUp() {
+		tile = new AnimatedTile(4);
+	}
+	
+	/**
+	 * Test AnimatedTile's setMerged method.
+	 */
+	@Test
+	public void testSetMerged() {
+		tile.setMerged(true);
+		assertTrue(tile.isMerged());
+	}
+	/**
+	 * Test AnimatedTile's reset method.
+	 */
+	@Test
+	public void testReset() {
+		tile.reset();
+		assertEquals(0, tile.getValue());
+		assertFalse(tile.isMerged());
+	}
+	/**
+	 * Test AnimatedTile's update method.
+	 */
+	@Test
+	public void testUpdate() {
+		tile.update();
+	}
+	/**
+	 * Test AnimatedTile's getTileSize method.
+	 */
+	@Test
+	public void testGetTileSize() {
+		assertEquals(81, tile.getTileSize(), .1f);
+	}
+	/**
+	 * Test AnimatedTile's getScale method.
+	 */
+	@Test
+	public void testGetScale() {
+		assertEquals(1, tile.getScale(), .1f);
+	}
+	/**
+	 * Test AnimatedTile's getXYOffset method.
+	 */
+	@Test
+	public void testGetXYOffset() {
+		assertEquals(0, tile.getXYOffset(), .1f);
+	}
+	/**
+	 * Test AnimatedTile's merge method.
+	 */
+	@Test
+	public void testMerge() {
+		tile.merge();
+	}
+
+}
