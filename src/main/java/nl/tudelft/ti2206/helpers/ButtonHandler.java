01183fc1e0932a10c16018e3536e1f017163c3e8
Jente Hidskes <hjdskes@gmail.com>
2014-09-13 12:14:05 +0200

    More abstraction between ButtonHandler and InputHandler

--- src/main/java/nl/tudelft/ti2206/helpers/ButtonHandler.java
+++ src/main/java/nl/tudelft/ti2206/helpers/ButtonHandler.java
@@ -2,9 +2,10 @@ package nl.tudelft.ti2206.helpers;
 
 import nl.tudelft.ti2206.buttons.ContinueButton;
 import nl.tudelft.ti2206.buttons.RestartButton;
+import nl.tudelft.ti2206.game.GameWorld;
 
 /**
- * The ButtonHandler is responsible of managing the buttons.
+ * The ButtonHandler is responsible for managing the buttons.
  *
  * @author group-21
  */
@@ -22,11 +23,33 @@ public class ButtonHandler {
 				AssetHandler.newgame.getY(), AssetHandler.newgame.getWidth(),
 				AssetHandler.newgame.getHeight(), AssetHandler.newgame,
 				AssetHandler.newgame);
-		
+
 		continueButton = new ContinueButton(AssetHandler.continuebutton.getX(),
 				AssetHandler.continuebutton.getY(), AssetHandler.continuebutton.getWidth(),
 				AssetHandler.continuebutton.getHeight(), AssetHandler.continuebutton,
 				AssetHandler.continuebutton);
+	}
+
+	/**
+	 * Delegates a touchDown event from the InputHandler to the correct button.
+	 * 
+	 * @param world A reference to the current GameWorld, so the buttons can interact with it.
+	 * @param screenX The x-coordinate of the touchDown event.
+	 * @param screenY The y-coordinate of the touchDown event.
+	 * @return True if the touchDown event is processed, false otherwise. 
+	 */
+	public static boolean touchDown(GameWorld world, int screenX, int screenY) {
+		if (restartButton.isTouchDown(screenX, screenY)) {
+			restartButton.onClick(world);
+			return true;
+		}
+
+		if (continueButton.isTouchDown(screenX, screenY)) {
+			continueButton.onClick(world);
+			return true;
+		}
+
+		return false;
 	}
 
 	/**
