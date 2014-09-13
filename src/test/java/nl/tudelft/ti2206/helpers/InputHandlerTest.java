3855a60fe5631a9d04459089a983e75e5df58525
Jente Hidskes <hjdskes@gmail.com>
2014-09-13 12:15:58 +0200

    Refactor InputHandlerTest
    1) Add JavaDoc wherever missing.
    2) Mock objects for better control- and readability.
    3) Add missing methods for better coverage.

--- src/test/java/nl/tudelft/ti2206/helpers/InputHandlerTest.java
+++ src/test/java/nl/tudelft/ti2206/helpers/InputHandlerTest.java
@@ -1,10 +1,9 @@
 package nl.tudelft.ti2206.helpers;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
-import nl.tudelft.ti2206.buttons.RestartButton;
 import nl.tudelft.ti2206.game.GameWorld;
-import nl.tudelft.ti2206.game.HeadlessLauncher;
 import nl.tudelft.ti2206.gameobjects.AnimatedGrid;
 import nl.tudelft.ti2206.gameobjects.Grid.Direction;
 
@@ -12,6 +11,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
 
 /**
@@ -21,33 +21,27 @@ import com.badlogic.gdx.Input.Keys;
  *
  */
 public class InputHandlerTest {
-
+	/** The object under test. */
 	private static InputHandler handler;
+	/** A mock to verify behavior. */
 	private static GameWorld world;
+	/** A mock to verify behavior. */
 	private static AnimatedGrid grid;
-	private static RestartButton restartButton;
 
 	/**
-	 * Initialize all (mock)objects and launch a headless game
-	 * 
-	 * @throws Exception
+	 * Initialize all mocks and create a new InputHandler.
 	 */
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		HeadlessLauncher launcher = new HeadlessLauncher();
-		launcher.launch();
-		world = new GameWorld();
-
+	public static void setUpBeforeClass() {
+		world = Mockito.mock(GameWorld.class);
 		grid = Mockito.mock(AnimatedGrid.class);
-		world.setGrid(grid);
+		when(world.getGrid()).thenReturn(grid);
 
-		restartButton = Mockito.mock(RestartButton.class);
 		handler = new InputHandler(world);
-		handler.setRestartButton(restartButton);
 	}
 
 	/**
-	 * Make sure the constructor works
+	 * Make sure the constructor works.
 	 */
 	@Test
 	public void testConstructor() {
@@ -55,68 +49,103 @@ public class InputHandlerTest {
 	}
 
 	/**
-	 * Make sure the move method is called with the correct parameter when a
-	 * specific arrow key is pressed
+	 * Make sure the move method is called with the correct parameter when the
+	 * down arrow key is pressed.
 	 */
 	@Test
-	public void testKeyDown1() {
+	public void testKeyDownDown() {
 		handler.keyDown(Keys.DPAD_DOWN);
 		verify(grid).move(Direction.DOWN);
 	}
 
 	/**
-	 * Make sure the move method is called with the correct parameter when a
-	 * specific arrow key is pressed
+	 * Make sure the move method is called with the correct parameter when the
+	 * up arrow key is pressed.
 	 */
 	@Test
-	public void testKeyDown2() {
+	public void testKeyDownUp() {
 		handler.keyDown(Keys.DPAD_UP);
 		verify(grid).move(Direction.UP);
 	}
 
 	/**
-	 * Make sure the move method is called with the correct parameter when a
-	 * specific arrow key is pressed
+	 * Make sure the move method is called with the correct parameter when the
+	 * left arrow key is pressed.
 	 */
 	@Test
-	public void testKeyDown3() {
+	public void testKeyDownLeft() {
 		handler.keyDown(Keys.DPAD_LEFT);
 		verify(grid).move(Direction.LEFT);
 	}
 
 	/**
-	 * Make sure the move method is called with the correct parameter when a
-	 * specific arrow key is pressed
+	 * Make sure the move method is called with the correct parameter when the
+	 * right arrow key is pressed.
 	 */
 	@Test
-	public void testKeyDown4() {
+	public void testKeyDownRight() {
 		handler.keyDown(Keys.DPAD_RIGHT);
 		verify(grid).move(Direction.RIGHT);
 	}
 
 	/**
-	 * Make sure the onClick method of RestartButton is called when Escape is
-	 * pressed. Comes down to restarting the game.
+	 * Make sure the keyDown method returns false when the key is invalid.
 	 */
 	@Test
-	public void testKeyDown5() {
-		handler.keyDown(Keys.ESCAPE);
-		verify(restartButton).onClick(world);
-		// reset to remove method invocation counter
-		reset(restartButton);
+	public void testKeyDownInvalid() {
+		assertEquals(false, handler.keyDown(Keys.BACKSLASH));
 	}
 
 	/**
-	 * Make sure the restartButton works as it should when it is pressed
+	 * Make sure the keyUp method returns false.
 	 */
 	@Test
-	public void testTouchDown() {
-		stub(restartButton.isTouchDown(0, 0)).toReturn(true);
+	public void testKeyUp() {
+		assertEquals(false, handler.keyUp(Keys.BACKSLASH));
+	}
 
-		handler.touchDown(0, 0, 0, 0);
+	/**
+	 * Make sure the keyTyped method returns false.
+	 */
+	@Test
+	public void testKeyTyped() {
+		assertEquals(false, handler.keyTyped('a'));
+	}
 
-		verify(restartButton).onClick(world);
-		// reset to remove method invocation counter
-		reset(restartButton);
+	/**
+	 * Make sure the mouseMoved method returns false.
+	 */
+	@Test
+	public void testMouseMoved() {
+		assertEquals(false, handler.mouseMoved(2, 3));
+	}
+
+	/**
+	 * Make sure the scrolled method returns false.
+	 */
+	@Test
+	public void testScrolled() {
+		assertEquals(false, handler.scrolled(5));
+	}
+
+//	@Test
+//	public void testTouchDown() {
+//		handler.touchDown(0, 0, 0, Buttons.LEFT);
+//	}
+
+	/**
+	 * Make sure the touchDragged method returns false.
+	 */
+	@Test
+	public void testTouchDragged() {
+		assertEquals(false, handler.touchDragged(2, 3, 5));
+	}
+
+	/**
+	 * Make sure the touchUp method returns false.
+	 */
+	@Test
+	public void testTouchUp() {
+		assertEquals(false, handler.touchUp(2, 3, 5, Buttons.LEFT));
 	}
 }
