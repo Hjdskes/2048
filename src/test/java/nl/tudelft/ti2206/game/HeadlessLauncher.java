7f82d7144e3a3560df0b567b9e16a51890ba41c8
Jente Hidskes <hjdskes@gmail.com>
2014-09-16 22:36:26 +0200

    Supple a new HeadlessLauncher and HeadlessTwentyFourtyGame

---
+++ src/test/java/nl/tudelft/ti2206/game/HeadlessLauncher.java
@@ -0,0 +1,40 @@
+package nl.tudelft.ti2206.game;
+
+import nl.tudelft.ti2206.game.Launcher;
+
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+
+/**
+ * This class launches an instance of TwentyFourtyGame, via a headless launcher.
+ * This is required for testing on the remote DevHub server.
+ * 
+ * @author group-21
+ */
+public class HeadlessLauncher extends Launcher {
+	/** The configuration for the game window. */
+	HeadlessApplicationConfiguration config;
+
+	/**
+	 * Main entry point for the game.
+	 * 
+	 * @param args
+	 *            The command line arguments. These are ignored.
+	 */
+	public static void main(final String[] args) {
+		HeadlessLauncher launcher = new HeadlessLauncher();
+		launcher.launch();
+	}
+
+	/**
+	 * Creates a new Launcher object. This object is automatically configured.
+	 */
+	public HeadlessLauncher() {
+		config = new HeadlessApplicationConfiguration();
+	}
+
+	@Override
+	public void launch() {
+		new HeadlessApplication(new HeadlessTwentyFourtyGame(), config);
+	}
+}
---
+++ src/test/java/nl/tudelft/ti2206/game/HeadlessTwentyFourtyGame.java
@@ -0,0 +1,38 @@
+package nl.tudelft.ti2206.game;
+
+import com.badlogic.gdx.ApplicationListener;
+
+/**
+ * The HeadlessTwentyFourtyGame is used to "mock" the real one, as to start the
+ * game without a screen and without calling AssetHandler.
+ * 
+ * @author group-21
+ */
+public class HeadlessTwentyFourtyGame implements ApplicationListener {
+	public HeadlessTwentyFourtyGame() {
+	}
+
+	@Override
+	public void create() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void render() {
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void resume() {
+	}
+}
