c571c66119c8ef3385871f7c13f92847b1707df7
pvanagtmaal <P.J.C.vanAgtmaal@student.tudelft.nl>
2014-09-18 14:39:56 +0200

    fonts altered to increase clarity

--- src/main/java/packer/Packer.java
+++ src/main/java/packer/Packer.java
@@ -4,6 +4,6 @@ import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 
 public class Packer {
     public static void main (String[] args) throws Exception {
-        TexturePacker.process("src/main/resources/images/icons", "src/main/resources/images/icons", "icons");
+        TexturePacker.process("src/main/resources/fonts/", "src/main/resources/fonts", "fonts");
     }
 }
