d152606436d61f6b7d8964b2fb4a68bfc3cdb1ad
Jente Hidskes <hjdskes@gmail.com>
2014-09-18 12:16:00 +0200

    Refactor ButtonHandler to ButtonDisplay.
    It now extends the Group class of LibGDX, allowing us to pack both
    buttons inside it and add it as a whole to the stage.
    
    No tests yet, as we have to figure out how to properly test this. We have
    tested it visually, so we are pretty sure it works correctly.

--- src/main/java/nl/tudelft/ti2206/gameobjects/ButtonDisplay.java
+++ src/main/java/nl/tudelft/ti2206/gameobjects/ButtonDisplay.java
@@ -1,39 +1,45 @@
-package nl.tudelft.ti2206.handlers;
+package nl.tudelft.ti2206.gameobjects;
 
 import nl.tudelft.ti2206.game.Game;
+import nl.tudelft.ti2206.handlers.AssetHandler;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
-public class ButtonHandler {
-
-	/** The width of the game */
+/**
+ * This class creates, positions and updates the buttons for restarting or
+ * continuing the game. It extends Group so an instance of this class can be
+ * added to the stage.
+ */
+public class ButtonDisplay extends Group {
+	/** The width of the game. */
 	private static final int GAME_WIDTH = Game.GAME_WIDTH;
+
 	/** The gap between the edge of the game and the button. */
 	private static final int GAP = 15;
-	/** The button width */
+
+	/** The button width. */
 	private static final int BUTTON_WIDTH = 140;
 
-	/** The button used to continue playing after the game is won. */
-	private static Button continueButton;
-	/** The button used to restart the game. */
-	private static Button restartButton;
+	/** The button height. */
+	private static final int BUTTON_HEIGHT = 70;
 
 	/**
 	 * Loads all buttons and sets their event listeners and locations.
 	 */
-	public static void load() {
+	public ButtonDisplay() {
 		initRestartButton();
 		initContinueButton();
 	}
-	
+
 	/**
-	 * Initializes the restart button
+	 * Initializes the restart button.
 	 */
-	private static void initRestartButton() {
-		restartButton = new Button(AssetHandler.getSkin()
-				.getDrawable("newgame"));
+	private void initRestartButton() {
+		Button restartButton = new Button(AssetHandler.getSkin().getDrawable(
+				"newgame"));
 		restartButton.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor) {
@@ -42,27 +48,23 @@ public class ButtonHandler {
 		});
 		restartButton.setX(GAME_WIDTH / 2 - BUTTON_WIDTH / 2);
 		restartButton.setY(GAP);
-	}
-	
-	private static void initContinueButton() {
-		continueButton = new Button(AssetHandler.getSkin().getDrawable(
-				"continue"));
-		// TODO: Set location
-	}
-	
-	/**
-	 * 
-	 * @return The continue button
-	 */
-	public static Button getContinuButton() {
-		return continueButton;
+		this.addActor(restartButton);
 	}
 
 	/**
-	 * 
-	 * @return The restart button.
+	 * Initializes the continue button.
 	 */
-	public static Button getRestartButton() {
-		return restartButton;
+	private void initContinueButton() {
+		Button continueButton = new Button(AssetHandler.getSkin().getDrawable(
+				"continue"));
+		continueButton.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				System.out.println("Continue");
+			}
+		});
+		continueButton.setX(GAME_WIDTH / 2 - BUTTON_WIDTH / 2);
+		continueButton.setY(BUTTON_HEIGHT + 2 * GAP);
+		this.addActor(continueButton);
 	}
 }
