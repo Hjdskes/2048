a359f4085a1f959bdc4d4e2af829b034b089e1ca
Jente Hidskes <hjdskes@gmail.com>
2014-09-19 10:11:32 +0200

    Renamed MultiPlayerScreen to MultiGameScreen

--- src/main/java/nl/tudelft/ti2206/screens/MultiGameScreen.java
+++ src/main/java/nl/tudelft/ti2206/screens/MultiGameScreen.java
@@ -4,9 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 
-public class MultiPlayerScreen implements Screen {
+public class MultiGameScreen implements Screen {
 
-	public MultiPlayerScreen() {
+	public MultiGameScreen() {
 	}
 
 	@Override
