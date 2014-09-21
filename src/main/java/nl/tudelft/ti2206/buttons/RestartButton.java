8e478a2edb858a7aca29832531db18b7e029c2ea
Jente Hidskes <hjdskes@gmail.com>
2014-09-21 15:10:13 +0200

    Remove unused imports

--- src/main/java/nl/tudelft/ti2206/buttons/RestartButton.java
+++ src/main/java/nl/tudelft/ti2206/buttons/RestartButton.java
@@ -4,7 +4,6 @@ import nl.tudelft.ti2206.game.TwentyFourtyGame;
 import nl.tudelft.ti2206.gameobjects.Grid;
 import nl.tudelft.ti2206.handlers.AssetHandler;
 
-import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
