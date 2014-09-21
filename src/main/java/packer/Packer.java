70ee4abac29afb751d3c7e15e7596fddc5266bb7
pvanagtmaal <P.J.C.vanAgtmaal@student.tudelft.nl>
2014-09-21 19:27:26 +0200

    ehmm... Now they are more beautiful :)

--- src/main/java/packer/Packer.java
+++ src/main/java/packer/Packer.java
@@ -4,6 +4,6 @@ import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 
 public class Packer {
     public static void main (String[] args) throws Exception {
-        TexturePacker.process("src/main/resources/fonts/", "src/main/resources/fonts", "fonts");
+        TexturePacker.process("src/main/resources/images/tiles", "src/main/resources/images/tiles", "tiles");
     }
 }
