674cf82af4c99884e9cf80bb1fe4e95f8fcec32a
Jente Hidskes <hjdskes@gmail.com>
2014-09-21 12:57:32 +0200

    Add a MultiLoseScreen and a MultiWinScreen

---
+++ src/main/java/nl/tudelft/ti2206/screens/MultiLoseScreen.java
@@ -0,0 +1,50 @@
+package nl.tudelft.ti2206.screens;
+
+import nl.tudelft.ti2206.buttons.MenuButton;
+import nl.tudelft.ti2206.handlers.AssetHandler;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+/**
+ * The MultiLoseScreen is displayed when the local player has won.
+ */
+public class MultiLoseScreen extends Screen {
+	/** The transparent background image. */
+	private Image image;
+
+	/** The button to go back to the menu. */
+	private MenuButton menuButton;
+
+	/** Constructs a new MultiLoseScreen. */
+	public MultiLoseScreen() {
+		stage = new Stage();
+		image = new Image(AssetHandler.getSkin(), "lostoverlay");
+		//image = new Image(AssetHandler.getSkin(), "multilostoverlay");
+		menuButton = new MenuButton();
+	}
+
+	/** Constructor used for mock insertion */
+	public MultiLoseScreen(Stage stage, Image image, MenuButton cancelButton) {
+		this.stage = stage;
+		this.image = image;
+		this.menuButton = cancelButton;
+	}
+
+	@Override
+	public void create() {
+		super.create();
+		stage.addActor(image);
+		stage.addActor(menuButton);
+	}
+
+	@Override
+	public void draw() {
+		stage.draw();
+	}
+
+	@Override
+	public boolean isOverlay() {
+		return true;
+	}
+}
