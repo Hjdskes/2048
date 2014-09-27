77ff03446c5349e74dc14c284075c570cce12003
Jente Hidskes <hjdskes@gmail.com>
2014-09-27 23:40:28 +0200

    Fix pom

--- src/main/java/packer/Packer.java
+++
@@ -1,9 +0,0 @@
-package packer;
-
-import com.badlogic.gdx.tools.texturepacker.TexturePacker;
-
-public class Packer {
-    public static void main (String[] args) throws Exception {
-        TexturePacker.process("src/main/resources/images/tiles", "src/main/resources/images/tiles", "tiles");
-    }
-}
