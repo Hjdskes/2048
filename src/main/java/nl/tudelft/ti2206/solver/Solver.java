693fed9a4738de534047555afeca3de1884fb6c4
pvanagtmaal <P.J.C.vanAgtmaal@student.tudelft.nl>
2014-10-10 20:03:17 +0200

    SolverTest updated

--- src/main/java/nl/tudelft/ti2206/solver/Solver.java
+++ src/main/java/nl/tudelft/ti2206/solver/Solver.java
@@ -11,27 +11,18 @@ import nl.tudelft.ti2206.gameobjects.Tile;
 
 public class Solver extends TimerTask {
 	private static final int[][] weightMatrices = {
-//		{  8,  7,  6,  5,
-//		   1,  2,  3,  4,
-//		  -4, -3, -2, -1, 
-//		  -5, -6, -7, -8
-//		},
-//		{ -5, -4,  1,  8,
-//		  -6, -3,  2,  7,
-//		  -7, -2,  3,  6,
-//		  -8, -1,  4,  5
-//		},
-		{ 8,  4,  1,  0,
-		  4,  1,  0, -1,
-		  1,  0, -1, -4,
-		  0, -1, -4, -8
-		},
-		{  0,  1,  4, 8,
-		  -1,  0,  1, 4,
-		  -4, -1,  0, 1,
-		  -8, -4, -1, 0
-		}
-	};
+			// { 8, 7, 6, 5,
+			// 1, 2, 3, 4,
+			// -4, -3, -2, -1,
+			// -5, -6, -7, -8
+			// },
+			// { -5, -4, 1, 8,
+			// -6, -3, 2, 7,
+			// -7, -2, 3, 6,
+			// -8, -1, 4, 5
+			// },
+			{ 8, 4, 1, 0, 4, 1, 0, -1, 1, 0, -1, -4, 0, -1, -4, -8 },
+			{ 0, 1, 4, 8, -1, 0, 1, 4, -4, -1, 0, 1, -8, -4, -1, 0 } };
 
 	private Grid original;
 	private int depth;
@@ -46,7 +37,8 @@ public class Solver extends TimerTask {
 	@Override
 	public void run() {
 		GameState state = TwentyFourtyGame.getState();
-		if (original.getPossibleMoves() == 0 || (state != GameState.CONTINUING &&  state != GameState.RUNNING)) {
+		if (original.getPossibleMoves() == 0
+				|| (state != GameState.CONTINUING && state != GameState.RUNNING)) {
 			this.cancel();
 		} else if (!calculating) {
 			System.out.println("Calculating next move...");
@@ -72,16 +64,17 @@ public class Solver extends TimerTask {
 
 	private double evaluate(Grid grid) {
 		double empty = getEmptyTilesNumber(grid.getTiles());
-		//double max = Math.pow(2, grid.getCurrentHighestTile());
-		//double monotonicity = getMonotonicity(grid.getTiles());
+		// double max = Math.pow(2, grid.getCurrentHighestTile());
+		// double monotonicity = getMonotonicity(grid.getTiles());
 		double gradient = getGradient(grid.getTiles());
-		//double smoothness = getSmoothness(grid.getTiles());
-		//return 0.4 * gradient + 0.1 * smoothness + 0.4 * empty + 0.1 * monotonicity;
-		//return 0.3 * gradient + 0.1 * smoothness + 0.6 * empty;
-		//return 0.4 * gradient + 0.1 * smoothness + 0.4 * empty + 0.1 * max;
-		//return 0.8 * gradient + 0.2 * smoothness;
-		//return 0.4 * gradient + 0.6 * empty; // GOOD
-		//return 0.6 * gradient + 0.4 * empty;
+		// double smoothness = getSmoothness(grid.getTiles());
+		// return 0.4 * gradient + 0.1 * smoothness + 0.4 * empty + 0.1 *
+		// monotonicity;
+		// return 0.3 * gradient + 0.1 * smoothness + 0.6 * empty;
+		// return 0.4 * gradient + 0.1 * smoothness + 0.4 * empty + 0.1 * max;
+		// return 0.8 * gradient + 0.2 * smoothness;
+		// return 0.4 * gradient + 0.6 * empty; // GOOD
+		// return 0.6 * gradient + 0.4 * empty;
 		return gradient;
 	}
 
@@ -106,9 +99,11 @@ public class Solver extends TimerTask {
 
 	private double playerMove(Grid grid, int depth) {
 		if (depth == 0) {
-			return grid.getPossibleMoves() > 0 ? evaluate(grid) : 0;
-		} else if (grid.getCurrentHighestTile() == 11) {
-			return Double.MAX_VALUE;
+			if (grid.getCurrentHighestTile() == 11) {
+				return Double.MAX_VALUE;
+			} else {
+				return grid.getPossibleMoves() > 0 ? evaluate(grid) : 0;
+			}
 		} else if (grid.getPossibleMoves() == 0) {
 			return Double.MIN_VALUE;
 		}
@@ -120,13 +115,13 @@ public class Solver extends TimerTask {
 				continue;
 			}
 
-//			double value = 0;
-//			if (cache.containsKey(clone.toString())) {
-//				value = cache.get(clone.toString()); 
-//			} else {
+			// double value = 0;
+			// if (cache.containsKey(clone.toString())) {
+			// value = cache.get(clone.toString());
+			// } else {
 			double value = computerMove(clone, depth - 1);
-//				cache.put(zobrist(clone.getTiles()), value);
-//			}
+			// cache.put(zobrist(clone.getTiles()), value);
+			// }
 
 			if (value > bestValue) {
 				bestValue = value;
@@ -139,19 +134,19 @@ public class Solver extends TimerTask {
 		if (depth == 0) {
 			return playerMove(grid, depth);
 		}
-		int[] possibleMoves =    { 1  , 2   };
+		int[] possibleMoves = { 1, 2 };
 		double[] probabilities = { 0.9, 0.1 };
 		double totalScore = 0;
 		double totalWeight = 0;
 
-		for(Tile tile : grid.getTiles()) {
+		for (Tile tile : grid.getTiles()) {
 			if (tile.isEmpty()) {
 				for (int i = 0; i < possibleMoves.length; i++) {
 					Grid clone = cloneGrid(grid);
 					clone.setTile(tile.getIndex(), possibleMoves[i]);
 
 					double value = playerMove(clone, depth - 1);
-					totalScore  += probabilities[i] * value;
+					totalScore += probabilities[i] * value;
 					totalWeight += probabilities[i];
 				}
 			}
@@ -159,14 +154,14 @@ public class Solver extends TimerTask {
 		return totalWeight == 0 ? 0 : totalScore / totalWeight;
 	}
 
-//	private long zobrist(Tile[] tiles) {
-//		long hash = 0xFFFFFFL;
-//
-//		for(Tile tile : tiles) {
-//			hash ^= tile.getValue();
-//		}
-//		return hash;
-//	}
+	// private long zobrist(Tile[] tiles) {
+	// long hash = 0xFFFFFFL;
+	//
+	// for(Tile tile : tiles) {
+	// hash ^= tile.getValue();
+	// }
+	// return hash;
+	// }
 
 	private double getGradient(Tile[] tiles) {
 		double best = 0;
@@ -183,38 +178,38 @@ public class Solver extends TimerTask {
 		return best;
 	}
 
-	private double getSmoothness(Tile[] tiles) {
-		double smoothness = 0;
-		double currentValue, targetValue;
-		Tile[] neighbors;
-
-		for (int i = 0; i < 16; i++) {
-			if (!tiles[i].isEmpty()) {
-				currentValue = tiles[i].getValue();
-				neighbors = getTileNeighbors(tiles, i);
-
-				for (Tile tile : neighbors) {
-					if (tile != null) {
-						targetValue = tile.getValue();
-						smoothness -= Math.abs(currentValue - targetValue);	
-					}
-				}
-			}
-		}
-		return smoothness;
-	}
-
-	private double getMonotonicity(Tile[] tiles) {
-		double mono = getMonotonicityHorizontal(tiles);
-		tiles = rotate(tiles, 90);
-		mono += getMonotonicityHorizontal(tiles);
-		tiles = rotate(tiles, 270);
-		return mono;
-	}
+	// private double getSmoothness(Tile[] tiles) {
+	// double smoothness = 0;
+	// double currentValue, targetValue;
+	// Tile[] neighbors;
+	//
+	// for (int i = 0; i < 16; i++) {
+	// if (!tiles[i].isEmpty()) {
+	// currentValue = tiles[i].getValue();
+	// neighbors = getTileNeighbors(tiles, i);
+	//
+	// for (Tile tile : neighbors) {
+	// if (tile != null) {
+	// targetValue = tile.getValue();
+	// smoothness -= Math.abs(currentValue - targetValue);
+	// }
+	// }
+	// }
+	// }
+	// return smoothness;
+	// }
+	//
+	// private double getMonotonicity(Tile[] tiles) {
+	// double mono = getMonotonicityHorizontal(tiles);
+	// tiles = rotate(tiles, 90);
+	// mono += getMonotonicityHorizontal(tiles);
+	// tiles = rotate(tiles, 270);
+	// return mono;
+	// }
 
 	private double getEmptyTilesNumber(Tile[] tiles) {
 		double res = 0;
-		for(int i = 0; i < tiles.length; i++) {
+		for (int i = 0; i < tiles.length; i++) {
 			if (tiles[i].isEmpty()) {
 				res++;
 			}
@@ -222,108 +217,108 @@ public class Solver extends TimerTask {
 		return (res == 0 ? res : Math.log(res));
 	}
 
-	private Tile[] getTileNeighbors(Tile[] tiles, int index) {
-		Tile[] neighbors = new Tile[2];
-
-		for (int offset = 1; offset < 4; offset += 1) {
-			if ((index + offset) % 4 != 0 && (index + offset) < tiles.length) {
-				if (!tiles[index + offset].isEmpty()) {
-					neighbors[0] = tiles[index + offset];
-					break;
-				}
-			}
-		}
-
-		for (int offset = 4; offset < 16; offset += 4) {
-			if ((index + offset) < tiles.length) {
-				if (!tiles[index + offset].isEmpty()) {
-					neighbors[1] = tiles[index + offset];
-					break;
-				}
-			}
-		}
-
-//		for (int offset = 1; offset < 4; offset += 1) {
-//			if ((index - offset) % 4 != 0 && (index - offset) >= 0) {
-//				if (!tiles[index - offset].isEmpty()) {
-//					neighbors[2] = tiles[index - offset];
-//					break;
-//				}
-//			}
-//		}
-//
-//		for (int offset = 4; offset < 16; offset += 4) {
-//			if ((index - offset) >= 0) {
-//				if (!tiles[index - offset].isEmpty()) {
-//					neighbors[3] = tiles[index - offset];
-//					break;
-//				}
-//			}
-//		}
-		return neighbors;
-	}
-
-	private double getMonotonicityHorizontal(Tile[] tiles) {
-		double[] totals = new double[2];
-		double currentValue, nextValue;
-
-		for (int y = 0; y < 4; y++) {
-			int current = 0;
-			int next = current + 1;
-			while (next < 4) {
-				while (next < 4 && tiles[next + 4 * y].isEmpty()) {
-					next++;
-				}
-				if (next >= 4) {
-					next--;
-				}
-				currentValue = tiles[current + 4 * y].getValue();
-				nextValue = tiles[next + 4 * y].getValue();
-				/* If the current is bigger, the order is decreasing.
-				 * Otherwise, it's increasing. */
-				if (currentValue > nextValue) {
-					totals[0] += nextValue - currentValue;
-				} else {
-					totals[1] += currentValue - nextValue;
-				}
-				current = next;
-				next++;
-			}
-		}
-
-		return Math.max(totals[0], totals[1]);
-	}
-
-	private Tile[] rotate(Tile[] tiles, int angle) {
-		Tile[] res = new Tile[16];
-
-		int offsetX = 3, offsetY = 3;
-		if (angle == 90) {
-			offsetY = 0;
-		} else if (angle == 270) {
-			offsetX = 0;
-		}
-
-		double rad = Math.toRadians(angle);
-		int cos = (int) Math.cos(rad);
-		int sin = (int) Math.sin(rad);
-		for (int x = 0; x < 4; x++) {
-			for (int y = 0; y < 4; y++) {
-				int newX = (x * cos) - (y * sin) + offsetX;
-				int newY = (x * sin) + (y * cos) + offsetY;
-				res[newX + newY * 4] = tiles[x + y * 4];
-			}
-		}
-		return res;
-	}
+	// private Tile[] getTileNeighbors(Tile[] tiles, int index) {
+	// Tile[] neighbors = new Tile[2];
+	//
+	// for (int offset = 1; offset < 4; offset += 1) {
+	// if ((index + offset) % 4 != 0 && (index + offset) < tiles.length) {
+	// if (!tiles[index + offset].isEmpty()) {
+	// neighbors[0] = tiles[index + offset];
+	// break;
+	// }
+	// }
+	// }
+	//
+	// for (int offset = 4; offset < 16; offset += 4) {
+	// if ((index + offset) < tiles.length) {
+	// if (!tiles[index + offset].isEmpty()) {
+	// neighbors[1] = tiles[index + offset];
+	// break;
+	// }
+	// }
+	// }
+	//
+	// // for (int offset = 1; offset < 4; offset += 1) {
+	// // if ((index - offset) % 4 != 0 && (index - offset) >= 0) {
+	// // if (!tiles[index - offset].isEmpty()) {
+	// // neighbors[2] = tiles[index - offset];
+	// // break;
+	// // }
+	// // }
+	// // }
+	// //
+	// // for (int offset = 4; offset < 16; offset += 4) {
+	// // if ((index - offset) >= 0) {
+	// // if (!tiles[index - offset].isEmpty()) {
+	// // neighbors[3] = tiles[index - offset];
+	// // break;
+	// // }
+	// // }
+	// // }
+	// return neighbors;
+	// }
+	//
+	// private double getMonotonicityHorizontal(Tile[] tiles) {
+	// double[] totals = new double[2];
+	// double currentValue, nextValue;
+	//
+	// for (int y = 0; y < 4; y++) {
+	// int current = 0;
+	// int next = current + 1;
+	// while (next < 4) {
+	// while (next < 4 && tiles[next + 4 * y].isEmpty()) {
+	// next++;
+	// }
+	// if (next >= 4) {
+	// next--;
+	// }
+	// currentValue = tiles[current + 4 * y].getValue();
+	// nextValue = tiles[next + 4 * y].getValue();
+	// /* If the current is bigger, the order is decreasing.
+	// * Otherwise, it's increasing. */
+	// if (currentValue > nextValue) {
+	// totals[0] += nextValue - currentValue;
+	// } else {
+	// totals[1] += currentValue - nextValue;
+	// }
+	// current = next;
+	// next++;
+	// }
+	// }
+	//
+	// return Math.max(totals[0], totals[1]);
+	// }
+	//
+	// private Tile[] rotate(Tile[] tiles, int angle) {
+	// Tile[] res = new Tile[16];
+	//
+	// int offsetX = 3, offsetY = 3;
+	// if (angle == 90) {
+	// offsetY = 0;
+	// } else if (angle == 270) {
+	// offsetX = 0;
+	// }
+	//
+	// double rad = Math.toRadians(angle);
+	// int cos = (int) Math.cos(rad);
+	// int sin = (int) Math.sin(rad);
+	// for (int x = 0; x < 4; x++) {
+	// for (int y = 0; y < 4; y++) {
+	// int newX = (x * cos) - (y * sin) + offsetX;
+	// int newY = (x * sin) + (y * cos) + offsetY;
+	// res[newX + newY * 4] = tiles[x + y * 4];
+	// }
+	// }
+	// return res;
+	// }
 
 	private Grid cloneGrid(Grid grid) {
 		Tile[] tiles = grid.getTiles();
 		Grid res = new Grid(true);
 		Tile[] newTiles = res.getTiles();
 
-		for(int i = 0; i < tiles.length; i++) {
-			newTiles[i] = new Tile(i, tiles[i].getValue()); 
+		for (int i = 0; i < tiles.length; i++) {
+			newTiles[i] = new Tile(i, tiles[i].getValue());
 		}
 		res.updateHighestTile();
 		return res;
--- src/test/java/nl/tudelft/ti2206/solver/SolverTest.java
+++ src/test/java/nl/tudelft/ti2206/solver/SolverTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 import nl.tudelft.ti2206.gameobjects.Grid;
 import nl.tudelft.ti2206.gameobjects.Grid.Direction;
 import nl.tudelft.ti2206.gameobjects.Tile;
+import nl.tudelft.ti2206.log.Logger;
+import nl.tudelft.ti2206.log.Logger.LogLevel;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,9 +14,11 @@ public class SolverTest {
 
 	private Grid grid;
 	private Solver solver;
+	private Logger logger = Logger.getInstance();
 
 	@Before
 	public void setUp() throws Exception {
+		logger.setLevel(LogLevel.NONE);
 		grid = new Grid(true);
 		grid.setTile(0, 2);
 		grid.setTile(1, 4);
@@ -35,6 +39,7 @@ public class SolverTest {
 		solver.run();
 		// Make sure the grid has changed
 		assertNotEquals(old, grid.toString());
+		solver.cancel();
 	}
 
 	@Test
@@ -42,29 +47,32 @@ public class SolverTest {
 		assertTrue(solver.scheduledExecutionTime() == 0);
 		solver.solve();
 		assertTrue(solver.scheduledExecutionTime() != 0);
+		solver.cancel();
 	}
 
 	@Test
 	public void testFindMoveHorizontal() {
-		grid.setTile(0, 1024);
-		grid.setTile(1, 1024);
+		grid.setTile(0, 10);
+		grid.setTile(1, 10);
 		Direction moveMade = solver.findMove(grid, 3);
 		boolean rightMoveMade = moveMade == Direction.LEFT
 				|| moveMade == Direction.RIGHT;
 		assertTrue(rightMoveMade);
+		solver.cancel();
 	}
 
 	// FIXME: make sure it makes a move down or up when 2 1024 tiles are located
 	// above each other
 
-	// @Test
-	// public void testFindMoveVertical() {
-	// grid.setTile(0, 1024);
-	// grid.setTile(4, 1024);
-	// Direction moveMade = solver.findMove(grid, 3);
-	// boolean rightMoveMade = moveMade == Direction.UP
-	// || moveMade == Direction.DOWN;
-	// assertTrue(rightMoveMade);
-	// }
+	@Test
+	public void testFindMoveVertical() {
+		grid.setTile(0, 10);
+		grid.setTile(4, 10);
+		Direction moveMade = solver.findMove(grid, 1);
+		boolean rightMoveMade = moveMade == Direction.UP
+				|| moveMade == Direction.DOWN;
+		assertTrue(rightMoveMade);
+		solver.cancel();
+	}
 
 }
