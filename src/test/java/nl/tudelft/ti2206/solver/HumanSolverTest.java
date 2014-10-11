f12279e16aae4404bb1eac652ca184bdf9126035
Jochem <J.W.Heijltjes@student.tudelft.nl>
2014-10-11 23:18:46 +0200

    Removed print statements in solvers and cleared Deliverable4.tex contents as this file is outdated and being updated.

--- src/test/java/nl/tudelft/ti2206/solver/HumanSolverTest.java
+++ src/test/java/nl/tudelft/ti2206/solver/HumanSolverTest.java
@@ -64,11 +64,6 @@ public class HumanSolverTest {
 		for (int i = 12; i < tiles.length; i++)
 			tiles[i].setValue(4);
 
-		// tiles[12].setValue(4);
-		// tiles[13].setValue(4);
-		// tiles[14].setValue(4);
-		// tiles[15].setValue(4);
-
 		assertTrue(HumanSolver.isMergePossibleLowerRow(grid));
 	}
 
@@ -92,7 +87,6 @@ public class HumanSolverTest {
 		// On an empty grid, nextMove() should not give us any points.
 		assertEquals(0, HumanSolver.nextMove(grid, 3));
 	}
-
 
 	/**
 	 * Test method for
@@ -130,30 +124,6 @@ public class HumanSolverTest {
 
 		assertEquals(Direction.LEFT, direction);
 	}
-
-//	/**
-//	 * Test method for
-//	 * {@link nl.tudelft.ti2206.solver.HumanSolver#selectDirectionComplex(nl.tudelft.ti2206.gameobjects.Grid, int)}
-//	 * .
-//	 */
-//	@Test
-//	public void testSelectDirectionComplexNull() {
-//
-//		grid.restart();
-//		grid.setTile(8, 64);
-//		grid.setTile(9, 128);
-//		grid.setTile(10, 128);
-//		grid.setTile(11, 16);
-//
-//		grid.setTile(12, 8);
-//		grid.setTile(13, 4);
-//		grid.setTile(14, 8);
-//		grid.setTile(15, 32);
-//
-//		Direction direction = HumanSolver.selectDirectionComplex(grid, 1);
-//
-//		assertEquals(null, direction);
-//	}
 
 	/**
 	 * Test method for
@@ -248,8 +218,6 @@ public class HumanSolverTest {
 		tiles[14].setValue(4);
 		tiles[15].setValue(8);
 
-		System.out.println(grid.toString());
-
 		assertFalse(HumanSolver.maxLowerLeft(grid));
 	}
 
@@ -267,8 +235,7 @@ public class HumanSolverTest {
 		}
 
 		grid.setTile(12, 4);
-		// tiles[12].setValue(4);
-		System.out.println("highest tile = " + grid.getCurrentHighestTile());
+
 		assertTrue(HumanSolver.maxLowerLeft(grid));
 	}
 
@@ -289,29 +256,4 @@ public class HumanSolverTest {
 
 	}
 
-	
-//	/**
-//	 * Test method for
-//	 * {@link nl.tudelft.ti2206.solver.HumanSolver#selectMove(nl.tudelft.ti2206.gameobjects.Grid, int)}
-//	 * .
-//	 */
-//	@Test
-//	public void testSelectMove2() {
-//		grid.restart();
-//		grid.setTile(8, 64);
-//		grid.setTile(9, 128);
-//		grid.setTile(10, 128);
-//		grid.setTile(11, 16);
-//
-//		grid.setTile(12, 8);
-//		grid.setTile(13, 4);
-//		grid.setTile(14, 8);
-//		grid.setTile(15, 32);
-//
-//		Direction direction = HumanSolver.selectMove(grid, 1);
-//
-//		assertEquals(Direction.LEFT, direction);
-//	}
-
-	
 }
