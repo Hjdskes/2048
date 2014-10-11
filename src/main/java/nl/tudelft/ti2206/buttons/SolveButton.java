93b90386507c197c2a9efb057871f211e1e5d26f
Jochem <J.W.Heijltjes@student.tudelft.nl>
2014-10-12 00:16:26 +0200

    Tied GridSolver/HumanSolver to SolveButton.

--- src/main/java/nl/tudelft/ti2206/buttons/SolveButton.java
+++ src/main/java/nl/tudelft/ti2206/buttons/SolveButton.java
@@ -4,7 +4,8 @@ import nl.tudelft.ti2206.drawables.DrawableTile;
 import nl.tudelft.ti2206.game.TwentyFourtyGame;
 import nl.tudelft.ti2206.gameobjects.Grid;
 import nl.tudelft.ti2206.handlers.AssetHandler;
-import nl.tudelft.ti2206.solver.Solver;
+import nl.tudelft.ti2206.solver.GridSolver;
+import nl.tudelft.ti2206.solver.GridSolver.Strategy;
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -14,26 +15,31 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
  * A button to call a solver to solve the game.
  */
 public class SolveButton extends TextButton {
-	private Solver solver;
+	private GridSolver gridSolver;
 
 	/** Constructs a new SolveButton. */
 	public SolveButton(final Grid grid) {
 		super("Solve", AssetHandler.getInstance().getSkin(), "small");
 		this.setHeight(50);
 		this.setWidth(DrawableTile.TILE_WIDTH);
-		this.setX(DrawableTile.TILE_X - DrawableTile.TILE_WIDTH / 2 - TwentyFourtyGame.GAP / 2);
+		this.setX(DrawableTile.TILE_X - DrawableTile.TILE_WIDTH / 2
+				- TwentyFourtyGame.GAP / 2);
 		this.setY(100 / 2 - this.getHeight() / 2);
 
+		gridSolver = new GridSolver(grid, Strategy.HUMAN, 750, 6);
+
 		this.addListener(new ClickListener() {
+
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				if (solver == null) {
-					solver = new Solver(grid, 7);
-					solver.solve();
+				if (gridSolver == null) {
+					gridSolver = new GridSolver(grid, Strategy.HUMAN, 750, 6);
+				}
+				if (gridSolver.isRunning()) {
+					gridSolver.stop();
 				} else {
-					solver.cancel();
-					solver = null;
-				};
+					gridSolver.start();
+				}
 			}
 		});
 	}
