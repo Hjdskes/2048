41fd8976b7701d1c209ff4a695fc1ddb6ef3cdf3
pvanagtmaal <P.J.C.vanAgtmaal@student.tudelft.nl>
2014-10-18 15:19:12 +0200

    Option to return to menu added to SettingsScreen (both Escape and a MenuButton)

--- src/main/java/nl/tudelft/ti2206/screens/menuscreens/SettingsScreen.java
+++ src/main/java/nl/tudelft/ti2206/screens/menuscreens/SettingsScreen.java
@@ -1,13 +1,19 @@
 package nl.tudelft.ti2206.screens.menuscreens;
 
+import nl.tudelft.ti2206.buttons.MenuButton;
 import nl.tudelft.ti2206.game.TwentyFourtyGame;
 import nl.tudelft.ti2206.handlers.AssetHandler;
+import nl.tudelft.ti2206.handlers.ScreenHandler;
 import nl.tudelft.ti2206.log.Logger;
 import nl.tudelft.ti2206.log.Logger.LogLevel;
 import nl.tudelft.ti2206.screens.Screen;
 import nl.tudelft.ti2206.screens.drawbehaviour.DrawBeige;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -22,6 +28,7 @@ public class SettingsScreen extends Screen {
 	private Slider slider;
 	private Label levelLabel;
 	private CheckBox checkBox;
+	private MenuButton menuButton;
 
 	/**
 	 * Creates a new SettingsScreen, setting up and adding all required actors
@@ -36,6 +43,7 @@ public class SettingsScreen extends Screen {
 				.getInstance().getSkin());
 		checkBox = new CheckBox("    Enable logging to file", AssetHandler
 				.getInstance().getSkin());
+		menuButton = new MenuButton();
 
 		setupSlider();
 		setupLevelLabel();
@@ -47,16 +55,23 @@ public class SettingsScreen extends Screen {
 	}
 	
 	/** Constructor for testing purposes only. */
-	public SettingsScreen(Slider slider, Label label, CheckBox checkBox) {
+	public SettingsScreen(MenuButton button, Slider slider, Label label, CheckBox checkBox) {
 		this.slider =  slider;
 		this.levelLabel = label;
 		this.checkBox = checkBox;
+		this.menuButton = button;
+		setupMenuButton();
 		setupLevelLabel();
 		setupCheckBox();
 		setListeners();
 		addActors();
 	}
 
+	private void setupMenuButton() {
+		menuButton.setX(10);
+		menuButton.setY(10);
+	}
+	
 	private void setupSlider() {
 		slider.setX(100);
 		slider.setY(350);
@@ -93,6 +108,23 @@ public class SettingsScreen extends Screen {
 				}
 			}
 		});
+		
+		// Return to main menu on escape
+		stage.addListener(new InputListener() {
+			@Override
+			public boolean keyDown(InputEvent event, int keycode) {
+				if (keycode == Keys.ESCAPE) {
+					Gdx.app.postRunnable(new Runnable() {
+						@Override
+						public void run() {
+							ScreenHandler.getInstance().set(new MenuScreen());
+						}
+					});
+					return true;
+				}
+				return false;
+			}
+		});
 	}
 
 	/** Adds the actors to the stage. */
@@ -100,26 +132,27 @@ public class SettingsScreen extends Screen {
 		stage.addActor(slider);
 		stage.addActor(levelLabel);
 		stage.addActor(checkBox);
+		stage.addActor(menuButton);
 	}
 
 	/** Updates the loglevel and returns the loglevel string. */
 	private String updateLevel() {
 		switch ((int) slider.getValue()) {
 		case 0:
-			logger.setLevel(LogLevel.ALL);
-			return "ALL";
+			logger.setLevel(LogLevel.NONE);
+			return "NONE";
 		case 100:
-			logger.setLevel(LogLevel.INFO);
-			return "INFO";
+			logger.setLevel(LogLevel.ERROR);
+			return "ERROR";
 		case 200:
 			logger.setLevel(LogLevel.DEBUG);
 			return "DEBUG";
 		case 300:
-			logger.setLevel(LogLevel.ERROR);
-			return "ERROR";
+			logger.setLevel(LogLevel.INFO);
+			return "INFO";
 		case 400:
-			logger.setLevel(LogLevel.NONE);
-			return "NONE";
+			logger.setLevel(LogLevel.ALL);
+			return "ALL";
 		}
 		return null;
 	}
