e2e101c4cf2cccec5ff8b0f95053fb80e2606b5a
paulbakker65 <paulbakker65@gmail.com>
2014-10-18 12:45:50 +0200

    Added javadoc to Local/RemoteInputHandlerTest

--- src/test/java/nl/tudelft/ti2206/handlers/LocalInputHandlerTest.java
+++ src/test/java/nl/tudelft/ti2206/handlers/LocalInputHandlerTest.java
@@ -12,10 +12,15 @@ import com.badlogic.gdx.Input.Keys;
 
 public class LocalInputHandlerTest {
 
+	/** The local Grid on which a move must be conducted. */
 	private Grid grid;
-	
+	/** The remoteInputHandler which takes remote input and we are going to test. */
 	private LocalInputHandler handler;
 
+	/**
+	 * Launches a headless launcher and creates the necessary objects before
+	 * each test.
+	 */
 	@Before
 	public void setUp() {
 		new HeadlessLauncher().launch();
@@ -23,33 +28,47 @@ public class LocalInputHandlerTest {
 		handler = new LocalInputHandler(grid);
 	}
 
+	/**
+	 * Checks if the current command is correctly set.
+	 */
 	@Test
 	public void testKeyDownDOWN() {
 		handler.keyDown(null, Keys.DPAD_DOWN);
 		assertTrue(handler.getCommand() instanceof MoveDownCommand);
 	}
 
+	/**
+	 * Checks if the current command is correctly set.
+	 */
 	@Test
 	public void testKeyDownUP() {
 		handler.keyDown(null, Keys.DPAD_UP);
 		assertTrue(handler.getCommand() instanceof MoveUpCommand);
 	}
 
+	/**
+	 * Checks if the current command is correctly set.
+	 */
 	@Test
 	public void testKeyDownLEFT() {
 		handler.keyDown(null, Keys.DPAD_LEFT);
 		assertTrue(handler.getCommand() instanceof MoveLeftCommand);
 	}
 
+	/**
+	 * Checks if the current command is correctly set.
+	 */
 	@Test
 	public void testKeyDownRIGHT() {
 		handler.keyDown(null, Keys.DPAD_RIGHT);
 		assertTrue(handler.getCommand() instanceof MoveRightCommand);
 	}
 
+	/**
+	 * Checks if nothing happens when an invalid key is pressed.
+	 */
 	@Test
 	public void testKeyDownNONE() {
-		// make sure no action is taken on an invalid key
 		assertFalse(handler.keyDown(null, Keys.A));
 	}
 }
--- src/test/java/nl/tudelft/ti2206/handlers/RemoteInputHandlerTest.java
+++ src/test/java/nl/tudelft/ti2206/handlers/RemoteInputHandlerTest.java
@@ -14,13 +14,20 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 public class RemoteInputHandlerTest {
-	
+
+	/** The remote Grid on which a move must be conducted. */
 	private Grid grid;
+	/** The remoteInputHandler which takes remote input and we are going to test. */
 	private RemoteInputHandler handler;
-	
+
+	/** Mocked logger to check if a update is received. */
 	@Mock
 	private Logger logger;
 
+	/**
+	 * Launches a headless launcher and creates the necessary objects before
+	 * each test.
+	 */
 	@Before
 	public void setUp() {
 		new HeadlessLauncher().launch();
@@ -31,6 +38,10 @@ public class RemoteInputHandlerTest {
 		handler.setLogger(logger);
 	}
 
+	/**
+	 * Test for the fillGrid() method. This test only fills the grid with two
+	 * tiles.
+	 */
 	@Test
 	public void testFillGrid() {
 		handler.fillGrid("2,11");
@@ -38,54 +49,73 @@ public class RemoteInputHandlerTest {
 		assertEquals(11, grid.getTiles()[1].getValue());
 	}
 
+	/**
+	 * Checks if the current command is correctly set.
+	 */
 	@Test
 	public void testMoveUp() {
 		handler.moveUp();
 		assertTrue(handler.getCommand() instanceof MoveUpCommand);
 	}
 
+	/**
+	 * Checks if the current command is correctly set.
+	 */
 	@Test
 	public void testMoveDown() {
 		handler.moveDown();
 		assertTrue(handler.getCommand() instanceof MoveDownCommand);
 	}
 
+	/**
+	 * Checks if the current command is correctly set.
+	 */
 	@Test
 	public void testMoveRight() {
 		handler.moveRight();
 		assertTrue(handler.getCommand() instanceof MoveRightCommand);
 	}
 
+	/**
+	 * Checks if the current command is correctly set.
+	 */
 	@Test
 	public void testMoveLeft() {
 		handler.moveLeft();
 		assertTrue(handler.getCommand() instanceof MoveLeftCommand);
 	}
 
+	/**
+	 * Checks if the update() method does work.
+	 */
 	@Test
 	public void testUpdate() {
 		handler.update(null, "MOVE[R]");
 		verify(logger).debug("RemoteInputHandler", "update received: MOVE[R]");
 	}
 
+	/**
+	 * Test for the validateGrid() method.
+	 */
 	@Test
 	public void testValidateGridValid() {
 		String validGrid = "2,4,0,2,0,2,8,16,16,2,4,1024,2048,2,2,2";
 		assertTrue(handler.validateGrid(validGrid));
 	}
 
+	/**
+	 * Test for the validateGrid() method.
+	 */
 	@Test
 	public void testValidateGridInValid() {
 		String validGrid = "2,4,0,2,0,2,8,16,,2,2";
 		assertFalse(handler.validateGrid(validGrid));
 	}
 
-	@Test
-	public void testHandleRemoteInputMove() {
-		handler.handleRemoteInput("MOVE[R]");
-		assertTrue(handler.getCommand() instanceof MoveRightCommand);
-	}
-
+	/**
+	 * Test for the handling of remote input. This test is with a valid grid.
+	 * The grid should be equal to the valid grid.
+	 */
 	@Test
 	public void testHandleRemoteInputValidGrid() {
 		String validGrid = "2,4,0,2,0,2,8,16,16,2,4,1024,2048,2,2,2";
@@ -93,6 +123,10 @@ public class RemoteInputHandlerTest {
 		assertEquals(validGrid, grid.toString());
 	}
 
+	/**
+	 * Test for the handling of remote input. This is with a invalid grid. The
+	 * logger should return a error message.
+	 */
 	@Test
 	public void testHandleRemoteInputInValidGrid() {
 		String invalidGrid = "2,4,0,2,0,2,8,16,16,2,4,,2048,2,2,2";
@@ -103,6 +137,10 @@ public class RemoteInputHandlerTest {
 						+ invalidGrid);
 	}
 
+	/**
+	 * Test for handling incorrect remote input. The logger should return an
+	 * error message.
+	 */
 	@Test
 	public void testHandleRemoteInputError() {
 		handler.handleRemoteInput("MOVE[R");
@@ -110,6 +148,10 @@ public class RemoteInputHandlerTest {
 				"Protocol parsing failed, no closing bracket found (-1).");
 	}
 
+	/**
+	 * Test for handling incorrect remote input. The logger should return an
+	 * error message.
+	 */
 	@Test
 	public void testHandleRemoteInputMoverError() {
 		handler.handleRemoteInput("MOVE[?]");
@@ -117,30 +159,50 @@ public class RemoteInputHandlerTest {
 				"Unknown remote direction parameter in protocol: ?");
 	}
 
+	/**
+	 * Test for handling of correct remote input. Checks if the current command
+	 * is correctly set.
+	 */
 	@Test
 	public void testHandleRemoteInputMoveLeft() {
 		handler.handleRemoteInput("MOVE[L]");
 		assertTrue(handler.getCommand() instanceof MoveLeftCommand);
 	}
 
+	/**
+	 * Test for handling of correct remote input. Checks if the current command
+	 * is correctly set.
+	 */
 	@Test
 	public void testHandleRemoteInputMoveRight() {
 		handler.handleRemoteInput("MOVE[R]");
 		assertTrue(handler.getCommand() instanceof MoveRightCommand);
 	}
 
+	/**
+	 * Test for handling of correct remote input. Checks if the current command
+	 * is correctly set.
+	 */
 	@Test
 	public void testHandleRemoteInputMoveUp() {
 		handler.handleRemoteInput("MOVE[U]");
 		assertTrue(handler.getCommand() instanceof MoveUpCommand);
 	}
 
+	/**
+	 * Test for handling of correct remote input. Checks if the current command
+	 * is correctly set.
+	 */
 	@Test
 	public void testHandleRemoteInputMoveDown() {
 		handler.handleRemoteInput("MOVE[D]");
 		assertTrue(handler.getCommand() instanceof MoveDownCommand);
 	}
 
+	/**
+	 * Test for handling of incorrect remote input. The logger should return an
+	 * error message.
+	 */
 	@Test
 	public void testHandleRemoteInputRandom() {
 		handler.handleRemoteInput("random]");
